249a23b55cea3309214830d712f3b0ba634bd8f8
Bruus Riezebos <bruus@riezebos.net>
2019-06-18 14:01:17 +0200

    Driver ClassException

--- src/main/java/net/riezebos/triviacouch/triviacouch/core/util/DataBase.java
+++ src/main/java/net/riezebos/triviacouch/triviacouch/core/util/DataBase.java
@@ -11,7 +11,8 @@ public class DataBase {
 
 		if (connection == null) {
 			try {
-				Connection conn = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5433", "triviacouch",
+				Class.forName("org.postgresql.Driver");
+				Connection conn = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5433/triviacouch", "triviacouch",
 						"trivia");
 				conn.setAutoCommit(false);
 				connection = conn;
