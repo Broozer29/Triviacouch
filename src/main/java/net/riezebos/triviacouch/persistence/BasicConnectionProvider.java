0a9a563bc9f974cc6749bf0ccf789c555b95df95
Bruus Riezebos <bruus@riezebos.net>
2019-06-29 20:37:39 +0200

    Properties niet in URL

--- src/main/java/net/riezebos/triviacouch/persistence/BasicConnectionProvider.java
+++ src/main/java/net/riezebos/triviacouch/persistence/BasicConnectionProvider.java
@@ -1,6 +1,5 @@
 package net.riezebos.triviacouch.persistence;
 
-import java.net.URI;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -14,25 +13,20 @@ public class BasicConnectionProvider implements ConnectionProvider {
 		try {
 			Class.forName("org.postgresql.Driver");
 
-			String databaseUrl = System.getenv("JDBC_DATABASE_URL");
-			System.out.println("JDBC_DATABASE_URL=" + databaseUrl);
+			String databaseUrl = System.getenv("DATABASE_URL");
+			String username = System.getenv("JDBC_DATABASE_USERNAME");
+			String password = System.getenv("JDBC_DATABASE_PASSWORD");
+			System.out.println("DATABASE_URL=" + databaseUrl);
+
 			if (databaseUrl == null)
-				throw new NullPointerException("Environment variabele JDBC_DATABASE_URL is null");
+				throw new NullPointerException("Environment variabele DATABASE_URL is null");
+			if (username == null)
+				throw new NullPointerException("Environment variabele JDBC_DATABASE_USERNAME is null");
+			if (password == null)
+				throw new NullPointerException("Environment variabele JDBC_DATABASE_PASSWORD is null");
 
-			URI dbUri = new URI(databaseUrl);
+			Connection conn = DriverManager.getConnection(databaseUrl, username, password);
 
-			String userInfo = dbUri.getUserInfo();
-			Connection conn;
-			if (userInfo != null) {
-				String username = userInfo.split(":")[0];
-				String password = userInfo.split(":")[1];
-				String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-				conn = DriverManager.getConnection(dbUrl, username, password);
-			} else {
-				// userid en ww onderdeel van path als property?
-				String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-				conn = DriverManager.getConnection(dbUrl);
-			}
 			conn.setAutoCommit(false);
 			return conn;
 		} catch (Exception e) {
