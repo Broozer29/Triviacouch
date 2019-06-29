af8fa1eec6c42707c360aad4ed442a77cb8f8e0d
Bruus Riezebos <bruus@riezebos.net>
2019-06-29 19:23:20 +0200

    Meer info bij url

--- src/main/java/net/riezebos/triviacouch/persistence/BasicConnectionProvider.java
+++ src/main/java/net/riezebos/triviacouch/persistence/BasicConnectionProvider.java
@@ -15,10 +15,10 @@ public class BasicConnectionProvider implements ConnectionProvider {
 			Class.forName("org.postgresql.Driver");
 
 			String databaseUrl = System.getenv("JDBC_DATABASE_URL");
-			if (databaseUrl == null) {
-				System.out.println("Environment variabele DATABASE_URL is niet gezet");
-				databaseUrl = "postgres://giwcxbsjzreveu:5855850949f513194c3403936e69f367ef5f896b6dc33c27702857d2b45b47af@ec2-54-228-246-214.eu-west-1.compute.amazonaws.com:5432/d94abodpb6rlmh";
-			}
+			System.out.println("JDBC_DATABASE_URL=" + databaseUrl);
+			if (databaseUrl == null)
+				throw new NullPointerException("Environment variabele JDBC_DATABASE_URL is null");
+
 			URI dbUri = new URI(databaseUrl);
 
 			String username = dbUri.getUserInfo().split(":")[0];
