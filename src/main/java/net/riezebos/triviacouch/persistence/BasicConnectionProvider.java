89b91a1a5100fd31e1193dca728c8e7dc341f086
Bruus Riezebos <bruus@riezebos.net>
2019-06-29 19:10:08 +0200

    Probeersel

--- src/main/java/net/riezebos/triviacouch/persistence/BasicConnectionProvider.java
+++ src/main/java/net/riezebos/triviacouch/persistence/BasicConnectionProvider.java
@@ -9,27 +9,29 @@ import java.sql.DriverManager;
  */
 public class BasicConnectionProvider implements ConnectionProvider {
 
- public Connection getConnection() {
+	public Connection getConnection() {
 
-  try {
-   Class.forName("org.postgresql.Driver");
+		try {
+			Class.forName("org.postgresql.Driver");
 
-   String databaseUrl = System.getenv("JDBC_DATABASE_URL");
-   if (databaseUrl == null)
-    throw new RuntimeException("Environment variabele DATABASE_URL is niet gezet");
-   URI dbUri = new URI(databaseUrl);
+			String databaseUrl = System.getenv("JDBC_DATABASE_URL");
+			if (databaseUrl == null) {
+				System.out.println("Environment variabele DATABASE_URL is niet gezet");
+				databaseUrl = "postgres://giwcxbsjzreveu:5855850949f513194c3403936e69f367ef5f896b6dc33c27702857d2b45b47af@ec2-54-228-246-214.eu-west-1.compute.amazonaws.com:5432/d94abodpb6rlmh";
+			}
+			URI dbUri = new URI(databaseUrl);
 
-   String username = dbUri.getUserInfo().split(":")[0];
-   String password = dbUri.getUserInfo().split(":")[1];
-   String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+			String username = dbUri.getUserInfo().split(":")[0];
+			String password = dbUri.getUserInfo().split(":")[1];
+			String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
-   Connection conn = DriverManager.getConnection(dbUrl, username, password);
+			Connection conn = DriverManager.getConnection(dbUrl, username, password);
 
-   conn.setAutoCommit(false);
-   return conn;
-  } catch (Exception e) {
-   throw new RuntimeException(e);
+			conn.setAutoCommit(false);
+			return conn;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 
-  }
- }
+		}
+	}
 }
